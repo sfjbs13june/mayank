f859cccc51addfa93202a8e850b9c87d65750f23
sfjbs <sfjbs@gmail.com>
2023-09-04 05:48:31 +0000

    commiting first microservice

---
+++ day02/first-web-microservice/src/main/java/com/mayank/app/firstwebmicroservice/FirstWebMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mayank.app.firstwebmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebMicroserviceApplication.class, args);
+	}
+
+}
---
+++ day02/first-web-microservice/src/test/java/com/mayank/app/firstwebmicroservice/FirstWebMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.app.firstwebmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
