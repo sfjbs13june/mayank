a49f3294f5e4b209b26e7e4936c7ee3b9ec7a393
sfjbs <sfjbs@gmail.com>
2023-09-05 04:07:34 +0000

    updating

---
+++ day02/first-micro-service/first-micro-service/src/main/java/com/mayank/app/FirstMicroServiceApplication.java
@@ -0,0 +1,13 @@
+﻿package com.mayank.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMicroServiceApplication.class, args);
+	}
+
+}
